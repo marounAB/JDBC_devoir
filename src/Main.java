9453063f6a9b96f110a1b2494c4eca755cb4ce3b
marounAB <maroun.abouboutros1@gmail.com>
2022-01-26 10:12:15 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		String url = "jdbc:mysql://localhost:3306/devoir";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			Connection cx = DriverManager.getConnection(url, "root", "12345");
+			Statement statement = cx.createStatement();
+			ResultSet result = statement.executeQuery("SELECT * FROM Students");
+			while (result.next()) {
+				System.out.println("firstname = " + result.getString("firstname") + " lastname = " + result.getString("lastname"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
